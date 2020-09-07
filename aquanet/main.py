4125353be3c27b26451e5c4ed6d2c8db040f7168
Denis <grabsomebuds27@gmail.com>
2020-09-07 14:51:15 +0300

    Fix mixin, add search by bages

--- aquanet/main.py
+++ aquanet/main.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-from mixin.page_data_mixin import PageDataMixin
+from mixin.page_data_mixin import PageDataMixin, MainPageData
 
-""" Header of csv file """
-with open('aquanet_2.csv', 'w') as f:
+""" Header of csv file outside iteration"""
+with open('aquanet.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(['Код', 'Артикул', 'Бренд', 'Коллекция', 'Установка', 'Ширина', 'Высота',
                      'Глубина', 'Материал', 'Дизайн', 'Цвет', 'Миксер', 'Гибкая подводка', 'Режимы', 'Радиус',
-                     'Термостат', 'Размер верхнего душа', 'Вес', 'Режимы верхнего душа'])
+                     'Термостат', 'Размер верхнего душа', 'Вес', 'Режимы верхнего душа', 'URL'])
 
 
 """ Headers by client request """
@@ -16,7 +16,15 @@ headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,imag
            "Accept-Encoding": "gzip, deflate, br",
            "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
            "Connection": "keep-alive",
-           "Cookie": "roistat_is_need_listen_requests=0; PHPSESSID=dkqigsqjnpog23oi06ve8e78cn; BITRIX_SM_SALE_UID=53262055; activity=0|20; _gcl_au=1.1.1930093311.1581921401; BITRIX_SM_VREGION_SUBDOMAIN=spb; BITRIX_SM_VREGION_SUBDOMAIN=spb; roistat_visit=778381; roistat_first_visit=778381; roistat_marker_old=; _ga=GA1.2.550948518.1581921402; _gid=GA1.2.680255184.1581921402; tmr_reqNum=5; tmr_lvid=76a44c8277ca4aa4a4b803049f61ad74; tmr_lvidTS=1581921402025; _ym_uid=1581921402466203911; _ym_d=1581921402; leadhunter_expire=1; _gat=1; _ym_visorc_23245510=w; _ym_isad=2; _fbp=fb.1.1581921402948.1181980815; ___dc=c8ad68ce-ab99-43f6-9729-12c2fe7a6b8b; tmr_detect=0%7C1581921404947; BX_USER_ID=0662459c6c9a798caa9c90b46efe2528; _gat_UA-44466602-1=1",
+           "Cookie": "roistat_is_need_listen_requests=0; PHPSESSID=dkqigsqjnpog23oi06ve8e78cn;"
+                     "BITRIX_SM_SALE_UID=53262055; activity=0|20; _gcl_au=1.1.1930093311.1581921401;"
+                     " BITRIX_SM_VREGION_SUBDOMAIN=spb; BITRIX_SM_VREGION_SUBDOMAIN=spb; roistat_visit=778381;"
+                     " roistat_first_visit=778381; roistat_marker_old=; _ga=GA1.2.550948518.1581921402;"
+                     " _gid=GA1.2.680255184.1581921402; tmr_reqNum=5; tmr_lvid=76a44c8277ca4aa4a4b803049f61ad74;"
+                     " tmr_lvidTS=1581921402025; _ym_uid=1581921402466203911; _ym_d=1581921402; leadhunter_expire=1;"
+                     " _gat=1; _ym_visorc_23245510=w; _ym_isad=2; _fbp=fb.1.1581921402948.1181980815; "
+                     "___dc=c8ad68ce-ab99-43f6-9729-12c2fe7a6b8b; tmr_detect=0%7C1581921404947;"
+                     " BX_USER_ID=0662459c6c9a798caa9c90b46efe2528; _gat_UA-44466602-1=1",
            "Host": "www.aquanet.ru",
            "Referer": "https://www.aquanet.ru/catalog/verkhnie_dushi/",
            "TE": "Trailers",
@@ -31,9 +39,9 @@ def get_html(url):
 
 
 def write_csv(data):
-    with open('aquanet_2.csv', 'a') as f:
+    """ Write data after pars """
+    with open('aquanet.csv', 'a') as f:
         writer = csv.writer(f)
-        # Нет шапки, для значений
         writer.writerow([data['uls_code'],
                          data['art'],
                          data['brand'],
@@ -48,11 +56,12 @@ def write_csv(data):
                          data['mixer'],
                          data['flexible_connection'],
                          data['watering_mode'],
-                         data['radius_l'],
+                         data['radius'],
                          data['termo'],
                          data['size_top'],
                          data['weigth'],
-                         data['watering_mode_top']])
+                         data['watering_mode_top'],
+                         data['url']])
 
 
 def get_page_data(response):
@@ -64,113 +73,62 @@ def get_page_data(response):
 
     for div in divs:
         """ First page """
-        name = div.select_one('p').get('title')
-        url = div.select_one('a').get('href')
+        url = MainPageData(div.select_one, 'a', 'href').find_main_page_data()
         urls.append('https://www.aquanet.ru' + url)
-        price = div.select_one('div.price').text
 
     for url in urls:
         """ 
             Page of each products
-            Request & pars param's 
+            Request & pars param's
+            Use PageDataMixin, for created short entry of exceptions
+            Use methods find_paragraph & find_links, for different param's of search text on page
         """
         r = requests.get(url)
         html = r.text
         soup = BeautifulSoup(html, 'lxml')
-        
-        uls_code = PageDataMixin(soup.select_one, 'ul.item_props', 'li', {'data-id': '470'}, 'p').find_paragraph()
-        print(uls_code)
-        art = PageDataMixin(soup.select_one, 'ul.item_props', 'li', {'data-id': '125'}, 'p').find_paragraph()
-        print(art)
 
-        try:
-            brand = soup.select_one('ul.item_props').find('li', attrs={'data-id': '126'}).find('a').text
-        except:
-            brand = 'empty'
-        try:
-            collections = soup.select_one('ul.item_props').find('li', attrs={'data-id': '226'}).find('a').text
-        except:
-            collections = 'empty'
-        try:
-            install = soup.select_one('ul.item_props').find('li', attrs={'data-id': '137'}).find('p').find_next('p').text
-        except:
-            install = 'empty'
-        try:
-            width = soup.select_one('ul.item_props').find('li', attrs={'data-id': '132'}).find('p').find_next('p').text
-        except:
-            width = 'empty'
-        try:
-            height = soup.select_one('ul.item_props').find('li', attrs={'data-id': '133'}).find('p').find_next('p').text
-        except:
-            height = 'empty'
-        try:
-            depth = soup.select_one('ul.item_props').find('li', attrs={'data-id': '134'}).find('p').find_next('p').text
-        except:
-            depth = 'empty'
-        try:
-            material = soup.select_one('ul.item_props').find('li', attrs={'data-id': '127'}).find('p').find_next('p').text
-        except:
-            material = 'empty'
-        try:
-            design = soup.select_one('ul.item_props').find('li', attrs={'data-id': '148'}).find('p').find_next('p').text
-        except:
-            design = 'empty'
-        try:
-            color = soup.select_one('ul.item_props').find('li', attrs={'data-id': '128'}).find('p').find_next('p').text
-        except:
-            color = 'empty'
-        try:
-            mixer = soup.select_one('ul.item_props').find('li', attrs={'data-id': '298'}).find('p').find_next('p').text
-        except:
-            mixer = 'empty'
-        try:
-            flexible_connection = soup.select_one('ul.item_props').find('li', attrs={'data-id': '283'}).find('p').find_next('p').text
-        except:
-            flexible_connection = 'empty'
-        try:
-            watering_mode = soup.select_one('ul.item_props').find('li', attrs={'data-id': '303'}).find('p').find_next('p').text
-        except:
-            watering_mode = 'empty'
-        try:
-            radius_l = soup.select_one('ul.item_props').find('li', attrs={'data-id': '306'}).find('p').find_next('p').text
-        except:
-            radius_l = 'empty'
-        try:
-            termo = soup.select_one('ul.item_props').find('li', attrs={'data-id': '155'}).find('p').find_next('p').text
-        except:
-            termo = 'empty'
-        try:
-            size_top = soup.select_one('ul.item_props').find('li', attrs={'data-id': '562'}).find('p').find_next('p').text
-        except:
-            size_top = 'empty'
-        try:
-            weigth = soup.select_one('ul.item_props').find('li', attrs={'data-id': '589'}).find('p').find_next('p').text
-        except:
-            weigth = 'empty'
-        try:
-            watering_mode_top = soup.select_one('ul.item_props').find('li', attrs={'data-id': '303'}).find('p').find_next('p').text
-        except:
-            watering_mode_top = 'empty'
+        """ Get properties from each pages """
+        uls_code = PageDataMixin(soup.select_one, {'data-id': '470'}).find_paragraph()
+        art = PageDataMixin(soup.select_one, {'data-id': '125'}).find_paragraph()
+        brand = PageDataMixin(soup.select_one, {'data-id': '126'}).find_links()
+        collections = PageDataMixin(soup.select_one, {'data-id': '226'}).find_links()
+        install = PageDataMixin(soup.select_one, {'data-id': '137'}).find_paragraph()
+        width = PageDataMixin(soup.select_one, {'data-id': '132'}).find_paragraph()
+        height = PageDataMixin(soup.select_one, {'data-id': '133'}).find_paragraph()
+        depth = PageDataMixin(soup.select_one, {'data-id': '134'}).find_paragraph()
+        material = PageDataMixin(soup.select_one, {'data-id': '127'}).find_paragraph()
+        design = PageDataMixin(soup.select_one, {'data-id': '148'}).find_paragraph()
+        color = PageDataMixin(soup.select_one, {'data-id': '128'}).find_paragraph()
+        mixer = PageDataMixin(soup.select_one, {'data-id': '298'}).find_paragraph()
+        flexible_connection = PageDataMixin(soup.select_one, {'data-id': '283'}).find_paragraph()
+        watering_mode = PageDataMixin(soup.select_one, {'data-id': '303'}).find_paragraph()
+        radius = PageDataMixin(soup.select_one, {'data-id': '306'}).find_paragraph()
+        termo = PageDataMixin(soup.select_one, {'data-id': '155'}).find_paragraph()
+        size_top = PageDataMixin(soup.select_one, {'data-id': '562'}).find_paragraph()
+        weigth = PageDataMixin(soup.select_one, {'data-id': '589'}).find_paragraph()
+        watering_mode_top = PageDataMixin(soup.select_one, {'data-id': '303'}).find_paragraph()
 
-        data = {'uls_code': uls_code.strip(),
-                'art': art.strip(),
-                'brand': brand.strip(),
-                'collections': collections.strip(),
-                'install': install.strip(),
-                'width': width.strip(),
-                'height': height.strip(),
-                'depth': depth.strip(),
-                'material': material.strip(),
-                'design': design.strip(),
-                'color': color.strip(),
-                'mixer': mixer.strip(),
-                'flexible_connection': flexible_connection.strip(),
-                'watering_mode': watering_mode.strip(),
-                'radius_l': radius_l.strip(),
-                'termo': termo.strip(),
-                'size_top': size_top.strip(),
-                'weigth': weigth.strip(),
-                'watering_mode_top': watering_mode_top.strip()}
+        # Write data to dict and get to the write csv func
+        data = {'uls_code': uls_code,
+                'art': art,
+                'brand': brand,
+                'collections': collections,
+                'install': install,
+                'width': width,
+                'height': height,
+                'depth': depth,
+                'material': material,
+                'design': design,
+                'color': color,
+                'mixer': mixer,
+                'flexible_connection': flexible_connection,
+                'watering_mode': watering_mode,
+                'radius': radius,
+                'termo': termo,
+                'size_top': size_top,
+                'weigth': weigth,
+                'watering_mode_top': watering_mode_top,
+                'url': url}
         write_csv(data)
 
 
@@ -178,6 +136,7 @@ def main():
     pattern = 'https://www.aquanet.ru/catalog/verkhnie_dushi/?PAGEN_1={}'
 
     for i in range(1, 16):
+        """ Loop requests to get response data from page on 15 pages   """
         url = pattern.format(str(i))
         get_page_data(get_html(url))
 
--- aquanet/mixin/page_data_mixin.py
+++ aquanet/mixin/page_data_mixin.py
@@ -3,22 +3,39 @@ from bs4 import BeautifulSoup
 
 
 class PageDataMixin:
-    """ Mixin for bs search data  """
-    def __init__(self, main_method, attrs, tag2, attrs2, tag3):
+    """ Mixin for bs search data on each page of products  """
+    def __init__(self, main_method, attrs):
         self.main_method = main_method
         self.attrs = attrs
-        self.tag2 = tag2
-        self.attrs2 = attrs2
-        self.tag3 = tag3
 
     def find_paragraph(self):
-        """ Find paragraphs """
+        """ Find paragraphs & links param's on page in each product """
         try:
-            return self.main_method(self.attrs).find(self.tag2, attrs=self.attrs2).find(self.tag3) \
-                .find_next(self.tag3).text
+            return self.main_method('ul.item_props').find('li', attrs=self.attrs).find('p').find_next('p').text.strip()
         except:
             "Empty"
 
+    def find_links(self):
+        """ Find links param's on page in each product """
+        try:
+            return self.main_method('ul.item_props').find('li', attrs=self.attrs).find('a').text.strip()
+        except:
+            "Empty"
+
+
+class MainPageData:
+    """ Find tags & attrs on main page """
+    def __init__(self, main_method, tag, attrs):
+        self.main_method = main_method
+        self.tag = tag
+        self.attrs = attrs
+
+    def find_main_page_data(self):
+        """ Find data on main page """
+        try:
+            return self.main_method(self.tag).get(self.attrs)
+        except:
+            "Empty"
 
 
 
